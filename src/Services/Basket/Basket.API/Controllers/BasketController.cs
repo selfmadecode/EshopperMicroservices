c73e73b74c8c36cb3b38a06a5fa9cb4f2ee7da31
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-24 10:11:47 +0100

    Added comment in basket

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -30,11 +30,18 @@ namespace Basket.API.Controllers
             return Ok(basket ?? new ShoppingCart(userName));
         }
 
+        // CREATE A BASKET OR UPDATE A BASKET FOR A USER
+        // ADD ITEMS INTO THE USER'S BASKET
+        // BEFORE SAVING, WE CONSUME DISCOUNT GRPC SERVICES TO GET DISCOUNT INFO FOR THE ITEM(s)
+        // MINUS DISCOUNT PRICES FOR SELECTED ITEMS
         [HttpPost]
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
             // TODO : Communicate with Discount.Grpc
+            // To setup the connection, bascket controller has to have the contract/proto file
+            // right click project, click add connected services, select grpc, locate the discount proto file
+
             // and Calculate latest prices of product into shopping cart
             // consume Discount Grpc
             //foreach (var item in basket.Items)
