f42f247f5f5a4e50d11f17b8ab756436ff156c09
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-24 10:56:00 +0100

    Created DiscountgRPCService class

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,29 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    // C# generated class DiscountProtoService.DiscountProtoServiceClient
+    // we did not inherit this class like we did in discount grpc because it is not a server class
+    // To see this class, show all files, obj, debug, net5.0, protos folder, click on discountgrpc.cs
+    // Was created after connecting to the proto file in discount and setting as client
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoServiceClient;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient)
+        {
+            _discountProtoServiceClient = discountProtoServiceClient ?? throw new ArgumentNullException(nameof(discountProtoServiceClient));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var getDiscountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProtoServiceClient.GetDiscountAsync(getDiscountRequest);
+        }
+    }
+}
