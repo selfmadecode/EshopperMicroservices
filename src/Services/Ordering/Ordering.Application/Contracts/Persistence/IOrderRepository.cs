6c7bd28f3dd50086ef6400e0c68dc09ad6db9b95
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-07-29 23:11:08 +0100

    Added additional method in IOrderRepository

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ordering.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Contracts.Persistence
 {
-    class IOrderRepository
+    public interface IOrderRepository : IAsyncRepository<Order>
     {
+        Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
     }
 }
