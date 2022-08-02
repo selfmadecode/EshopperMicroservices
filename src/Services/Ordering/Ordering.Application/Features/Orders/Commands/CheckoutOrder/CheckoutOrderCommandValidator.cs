a5856b28b61ee48d7bc0c500088876ecd69b8b34
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-02 23:17:36 +0100

    Added rule validation for email address

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -14,6 +14,9 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
             RuleFor(x => x.UserName).NotEmpty()
                 .WithMessage("{UserName} is required").NotNull()
                 .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters");
+
+            RuleFor(x => x.EmailAddress).NotEmpty()
+                .WithMessage("{EmailAddress} is required").NotNull();
         }
     }
 }
