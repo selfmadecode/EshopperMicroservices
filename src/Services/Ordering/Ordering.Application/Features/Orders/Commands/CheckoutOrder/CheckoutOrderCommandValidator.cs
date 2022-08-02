24e27bc634326529981252a665fc1e3057fb2efc
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-02 23:16:01 +0100

    Added Validation rules for email

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
 {
-    public class CheckoutOrderCommandValidator
+    public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
     {
+        public CheckoutOrderCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty()
+                .WithMessage("{UserName} is required").NotNull()
+                .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters");
+        }
     }
 }
