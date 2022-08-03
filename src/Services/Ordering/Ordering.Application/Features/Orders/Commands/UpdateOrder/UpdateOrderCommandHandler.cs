29a804fa7af1da7551f1407286866d92f83e2662
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-03 23:27:32 +0100

    Added Mapper and Logger Interface

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -1,13 +1,37 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
 {
     public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
     {
+        private readonly IOrderRepository _repo;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(IOrderRepository repo, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _repo.GetByIdAsync(request.Id);
+
+            if(orderToUpdate == null)
+            {
+
+            }
+        }
     }
 }
