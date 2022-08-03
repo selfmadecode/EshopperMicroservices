6fa939c9c282b704526b366acef25de6b857dba1
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-03 23:09:14 +0100

    Added Id to UpdateOrder

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -8,6 +8,7 @@ namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
 {
     public class UpdateOrderCommand
     {
+        public Guid Id { get; set; }
         public string UserName { get; set; }
         public decimal TotalPrice { get; set; }
 
