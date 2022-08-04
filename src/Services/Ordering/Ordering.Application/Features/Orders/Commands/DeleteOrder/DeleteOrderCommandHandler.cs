96c994cb4c7a5c067f9a16423bfd0aa81413ff47
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-04 23:38:47 +0100

    removed unnecessary using statement

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -2,9 +2,6 @@
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Persistence;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
