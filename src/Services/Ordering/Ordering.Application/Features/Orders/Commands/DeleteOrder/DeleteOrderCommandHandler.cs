2b1f3f7cf775c76c079a868ca52f01867b18528b
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-04 23:11:56 +0100

    Added Handle method for Delete order handler

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 {
-    public class DeleteOrderCommandHandler
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
     {
+        public Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
