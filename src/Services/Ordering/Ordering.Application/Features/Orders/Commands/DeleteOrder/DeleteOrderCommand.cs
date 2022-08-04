1b2bf96ced91d24e47063b0063a596e66e966f52
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-04 23:10:16 +0100

    Added Id to delete order

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
 {
-    public class DeleteOrderCommand
+    public class DeleteOrderCommand : IRequest
     {
+        public Guid Id { get; set; }
     }
 }
