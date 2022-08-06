61385d1bc0f01fc207551e900f5c40f722de46fa
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-06 23:16:03 +0100

    Added ValidationException

--- src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using FluentValidation.Results;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Ordering.Application.Exceptions
 {
     public class ValidationException : ApplicationException
     {
+        public IDictionary<string, string[]> Error { get; }
+
         public ValidationException()
             : base("One or more validation failures have occured.")
         {
             Error = new Dictionary<string, string[]>();
         }
 
-        public IDictionary<string, string[]> Error { get;}
+        public ValidationException(IEnumerable<ValidationFailure> failures) // validationfaluires comes from fluentAPI
+            :this()
+        {
+            Error = failures.GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
     }
 }
