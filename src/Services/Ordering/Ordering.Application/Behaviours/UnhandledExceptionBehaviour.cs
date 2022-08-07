1933225c3c118f8afd02e67c92e9342c5cfee5dc
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-07 23:34:41 +0100

    Logged Exception

--- src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -15,19 +15,28 @@ namespace Ordering.Application.Behaviours
     {
 
         private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
+            // GLOBAL EXCEPTION HANDLING FOR AND HANDLE METHODS
+            // NO NEED TO H=WRAP ALL HANDLE METHOD IN A TRY-CATCH
             try
             {
+                return await next();
 
             }
             catch (Exception ex)
             {
-
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Unhandled Exception for request {Name} {@RequestName}", requestName, request);
                 throw;
             }
 
-            return await next();
         }
     }
 }
