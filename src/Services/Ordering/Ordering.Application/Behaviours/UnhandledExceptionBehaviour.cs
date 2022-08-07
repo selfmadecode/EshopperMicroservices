10d1a91f2727730999e4bf98c2f59b2e4bed9ea0
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-07 23:27:50 +0100

    Added Log

--- src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +13,20 @@ namespace Ordering.Application.Behaviours
         where TRequest : IRequest<TResponse> // MediatR 9.5.3 does not have this
 
     {
+
+        private readonly ILogger<UnhandledExceptionBehaviour> _logger;
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
             return await next();
         }
     }
