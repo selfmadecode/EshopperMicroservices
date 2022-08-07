ef40d2b96b7e8a03f9185d8abd417384ed68ea44
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-07 23:05:17 +0100

    Added where clause

--- src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using FluentValidation;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ordering.Application.Behaviours
 {
     // intercepts the request and runs the validation rules associated with the request
-    public class ValidationBehaviour
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
     {
+        private readonly IEnumerable<IValidator<TRequest>> validators;
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
