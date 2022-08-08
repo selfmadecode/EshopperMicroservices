4bc812b7094da80f09a0a63933e8d6f640151853
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-08 23:10:02 +0100

    Registered Automapper

--- src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace Ordering.Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
             return services;
         }
     }
