95be08036ef7d1c5d3c7dbc356ce677afd7e04a3
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-09 23:29:47 +0100

    removed unnessary using statement

--- src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
@@ -2,12 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Behaviours;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ordering.Application
 {
@@ -20,7 +15,7 @@ namespace Ordering.Application
 
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Register fluent validators
 
-            // using reflection, fluent will look for classes that inherits from AbstractValidator
+            // using reflection, fluent validator will look for classes that inherits from AbstractValidator
             services.AddMediatR(Assembly.GetExecutingAssembly()); // Register mediatR
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
