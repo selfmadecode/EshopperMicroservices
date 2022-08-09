f7f33f668b77f5ba7d0ca187ba4999771227c818
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-09 23:26:45 +0100

    Added comment for automapper registration

--- src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
@@ -16,7 +16,10 @@ namespace Ordering.Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            // using reflection, automapper will look for classes that inherits from Profile 
+
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Register fluent validators
+
             // using reflection, fluent will look for classes that inherits from AbstractValidator
             services.AddMediatR(Assembly.GetExecutingAssembly()); // Register mediatR
 
