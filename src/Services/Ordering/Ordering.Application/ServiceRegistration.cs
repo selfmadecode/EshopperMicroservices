fe0ffa8d33ced0c1f0d42f11618d6e855200c3b4
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-09 23:26:00 +0100

    Added comment

--- src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
+++ src/Services/Ordering/Ordering.Application/ServiceRegistration.cs
@@ -17,6 +17,7 @@ namespace Ordering.Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly()); // Register fluent validators
+            // using reflection, fluent will look for classes that inherits from AbstractValidator
             services.AddMediatR(Assembly.GetExecutingAssembly()); // Register mediatR
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
