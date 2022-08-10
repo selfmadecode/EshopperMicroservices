d947189a6818a0832e8fc7a1cd2b886388cf9218
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-10 23:05:43 +0100

    Added getOrders

--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Ordering.API.Controllers
@@ -17,6 +19,12 @@ namespace Ordering.API.Controllers
         public OrderController(IMediator mediator)
         {
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<OrdersVm>>> GetOrders()
+        {
+            
         }
     }
 }
