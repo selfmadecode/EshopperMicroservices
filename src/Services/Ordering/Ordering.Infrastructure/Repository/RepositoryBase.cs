684b98a459239bea99d9a4c24730e71c7a4878ad
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-12 00:06:33 +0100

    Added GetAllAsync

--- src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
@@ -1,9 +1,25 @@
-﻿using Ordering.Application.Contracts.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
 using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Repository
 {
     public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
     {
+        protected readonly OrderContext _dbContext;
+
+        public RepositoryBase(OrderContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
     }
 }
