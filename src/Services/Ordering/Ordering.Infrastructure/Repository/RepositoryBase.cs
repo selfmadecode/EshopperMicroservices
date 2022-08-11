4f1176d040e41b516a97e0beb27c144bf6872f82
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-12 00:23:31 +0100

    Implemented GetAsync

--- src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
@@ -28,5 +28,25 @@ namespace Ordering.Infrastructure.Repository
         {
             return await _dbContext.Set<T>().Where(predicate).ToListAsync();
         }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                                        string includeString = null, bool disableTracking = true)
+        {
+            IQueryable<T> query = _dbContext.Set<T>();
+
+            if (disableTracking)
+                query = query.AsNoTracking();
+
+            if (!String.IsNullOrWhiteSpace(includeString))
+                query = query.Include(includeString);
+
+            if (predicate != null)
+                query = query.Where(predicate);
+
+            if (orderBy != null)
+                return await orderBy(query).ToListAsync();
+
+            return await query.ToListAsync();
+        }
     }
 }
