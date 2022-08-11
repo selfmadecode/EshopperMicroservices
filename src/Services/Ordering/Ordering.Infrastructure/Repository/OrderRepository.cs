0731aaadc5cee586a75db04d8c378fbce6a70892
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-12 00:42:10 +0100

    Added OrderContext to base

--- src/Services/Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repository/OrderRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Ordering.Infrastructure.Repository
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repository
 {
-    public class OrderRepository
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
     {
+        public OrderRepository(OrderContext context) : base(context)
+        {
+
+        }
     }
 }
