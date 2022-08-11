e9e02a10b4fcea63d3d0b03bcea06aff05bef4c4
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-11 16:59:22 +0100

    Added SeedOrder class

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/SeedOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class SeedOrder
+    {
+        public static async Task SeedOrder(OrderContext context, ILogger<SeedOrder> logger)
+        {
+            if (!context.Orders.Any())
+            {
+                var newOrders = new List<Order>()
+                {
+                    
+                };
+                await context.Orders.AddRangeAsync(newOrders);
+                await context.SaveChangesAsync();
+            }
+
+           
+        }
+    }
+}
