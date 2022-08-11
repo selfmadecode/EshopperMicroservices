83e59a3bf07ba108634df0bdcc1ff441de314c78
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-11 23:44:29 +0100

    removed unnecessary using statements

--- src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Repository/RepositoryBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ordering.Infrastructure.Repository
+﻿namespace Ordering.Infrastructure.Repository
 {
     public class RepositoryBase
     {
