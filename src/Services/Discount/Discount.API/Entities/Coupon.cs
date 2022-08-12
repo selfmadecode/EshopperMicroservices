702df2b3cd33c6ed706df7e2de56291d0e1a1f87
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-12 22:43:27 +0100

    Removed unused using statements

--- src/Services/Discount/Discount.API/Entities/Coupon.cs
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Discount.API.Entities
+﻿namespace Discount.API.Entities
 {
     public class Coupon
     {
