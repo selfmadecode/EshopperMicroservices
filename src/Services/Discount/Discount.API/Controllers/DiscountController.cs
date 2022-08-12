96e8d6cb8cad5183ba4788f7184b27afc1196ebc
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-12 22:43:22 +0100

    Removed unused using statements

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -1,10 +1,7 @@
 ﻿using Discount.API.Entities;
 using Discount.API.Repositories;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
