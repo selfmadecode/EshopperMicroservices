062e2bdbf9e1136f32a36408b7be0ed5efd15afd
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-14 15:29:51 +0100

    registered application services

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -20,7 +20,7 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddApplicationServices();
+            services.AddApplicationServices();
             //services.AddInfrastructureServices();
 
             services.AddControllers();
