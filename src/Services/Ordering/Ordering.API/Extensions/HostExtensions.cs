bede5c3cf00e4428e62cc7848abb488d4bc67780
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-15 23:16:03 +0100

    Added required using statements

--- src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace Ordering.API.Extensions
@@ -9,7 +11,24 @@ namespace Ordering.API.Extensions
         public static IHost MigrateDatabase<TContext>(this IHost host,
             Action<TContext, IServiceProvider> seeder, int? retry = 0) where TContext : DbContext
         {
+            int retryForAvailability = retry.Value;
 
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                                    }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+            return host;
         }
+    }
     }
 }
