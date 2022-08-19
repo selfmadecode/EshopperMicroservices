9f9ebf980363699417b2091ec059ee1e7d452c16
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-19 18:14:49 +0100

    Added IntegrationBaseEvent

---
+++ src/BuildingBlocks/EventBus.Message/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Message.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public IntegrationBaseEvent()
+        {
+            Id = new Guid();
+            CreatedOn = DateTime.Now;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createdOn)
+        {
+            Id = id;
+            CreatedOn = createdOn;
+        }
+    }
+}
