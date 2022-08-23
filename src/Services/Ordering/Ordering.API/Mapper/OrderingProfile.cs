199b23a0e92073c58619e8d297f9d0ea5a7b4958
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-24 00:03:28 +0100

    Created mapping profile for CheckoutOrderCommand and BasketCheckoutEvent

--- src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
+++ src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using EventBus.Message.Events;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,9 @@ namespace Ordering.API.Mapper
 {
     public class OrderingProfile : Profile
     {
+        public OrderingProfile()
+        {
+            CreateMap<BasketCheckoutEvent, CheckoutOrderCommand>().ReverseMap();
+        }
     }
 }
