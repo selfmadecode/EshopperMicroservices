db07165b202840648f386e13f1207e426aa97c8c
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-24 00:19:48 +0100

    modified consume method

--- src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -3,6 +3,7 @@ using EventBus.Message.Events;
 using MassTransit;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
 using System;
 using System.Threading.Tasks;
 
@@ -14,9 +15,20 @@ namespace Ordering.API.EventBusConsumer
         private readonly IMapper _mapper;
         private readonly IMediator _mediator;
         private readonly ILogger<BasketCheckoutConsumer> _logger;
-        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
         {
-            throw new NotImplementedException();
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = _mapper.Map<CheckoutOrderCommand>(context.Message);
+            var result = await _mediator.Send(command);
+
+            _logger.LogInformation($"BasketCheckoutEvent consumed, orderId: {result}");
         }
     }
 }
