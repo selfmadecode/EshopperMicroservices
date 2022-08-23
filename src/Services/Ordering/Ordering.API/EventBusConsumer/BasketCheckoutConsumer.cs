d6aef36f44ecf7f8bf94588650b73c26b07d111f
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-23 23:49:05 +0100

    implemented consume method

--- src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -10,5 +10,9 @@ namespace Ordering.API.EventBusConsumer
     // The IConsumer class is need to make basketcheckoutConsumer the subscriber of BasketCheckoutEvent
     public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
     {
+        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
