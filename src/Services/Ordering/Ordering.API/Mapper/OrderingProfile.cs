8a0b03942ea3b8dd5fa1874d73fb82055a0a41f0
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-24 00:03:40 +0100

    removed using

--- src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
+++ src/Services/Ordering/Ordering.API/Mapper/OrderingProfile.cs
@@ -1,10 +1,6 @@
 ﻿using AutoMapper;
 using EventBus.Message.Events;
 using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Ordering.API.Mapper
 {
