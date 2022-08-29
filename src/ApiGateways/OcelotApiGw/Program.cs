76369d409179ca9c3b80879d1b410d82b95c8f7a
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-08-30 00:45:48 +0100

    modified json file config

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -20,7 +20,7 @@ namespace OcelotApiGw
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) => 
                 {
-                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json");
+                    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
