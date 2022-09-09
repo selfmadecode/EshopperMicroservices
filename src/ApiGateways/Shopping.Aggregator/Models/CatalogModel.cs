dcb2d7e620b85cd6114880e94fbe39995630cd4b
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-09 22:29:40 +0100

    Renamed product to CatalogModel

--- src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Shopping.Aggregator.Model
 {
-    public class Product
+    public class CatalogModel
     {
         public string Id { get; set; }
         public string Name { get; set; }
