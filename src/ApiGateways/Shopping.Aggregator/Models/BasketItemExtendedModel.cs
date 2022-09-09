76dff35c304a6675bf166a9d3c63eac020f41f7b
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-09 22:35:55 +0100

    Added basket properties

--- src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketItemExtendedModel.cs
@@ -7,5 +7,12 @@ namespace Shopping.Aggregator.Models
 {
     public class BasketItemExtendedModel
     {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+
     }
 }
