5c629e3c52096209979053180fd640dc758d40bf
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-12 22:43:14 +0100

    Changed namespace for CatalogService

--- src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -3,12 +3,20 @@ using Shopping.Aggregator.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Shopping.Aggregator
+namespace Shopping.Aggregator.Services
 {
     public class CatalogService : ICatalogService
     {
+        private readonly HttpClient _client;
+
+        public CatalogService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
         public Task<IEnumerable<CatalogModel>> GetCatalog()
         {
             throw new NotImplementedException();
