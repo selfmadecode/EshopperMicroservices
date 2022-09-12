d2b52794f2588dc570a9879f4c52ee18d583066d
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-13 00:14:27 +0100

    Injected HttpClient into BasketService

--- src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -1,11 +1,18 @@
 ﻿using Shopping.Aggregator.Models;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Shopping.Aggregator.Services
 {
     public class BasketService : IBasketService
     {
+        private readonly HttpClient _client;
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
         public Task<BasketModel> GetBasket(string userName)
         {
             throw new NotImplementedException();
