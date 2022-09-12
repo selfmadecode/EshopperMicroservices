5ee7efd1424dbc0a3a012a6731bb336c91159031
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-12 22:34:44 +0100

    Implemented IOrderService Interface

--- src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shopping.Aggregator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace Shopping.Aggregator.Services
 {
     public class OrderService : IOrderService
     {
+        public Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
