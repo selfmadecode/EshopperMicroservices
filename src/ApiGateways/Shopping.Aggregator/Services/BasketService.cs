1941613bf8d135da9a5de3e3ab489e883299a5e8
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-12 22:18:39 +0100

    removed unused using statement

--- src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Shopping.Aggregator.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace Shopping.Aggregator.Services
 {
     public class BasketService : IBasketService
     {
+        public Task<BasketModel> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
