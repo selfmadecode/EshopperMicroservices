48e149c1baae9a38e847cd950e35985b9316e228
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-12 22:34:25 +0100

    Implemented ICatalog Interface

--- src/ApiGateways/Shopping.Aggregator/CatalogService.cs
+++ src/ApiGateways/Shopping.Aggregator/CatalogService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shopping.Aggregator.Model;
+using Shopping.Aggregator.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,19 @@ namespace Shopping.Aggregator
 {
     public class CatalogService : ICatalogService
     {
+        public Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CatalogModel> GetCatalog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
