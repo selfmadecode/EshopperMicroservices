7074298387120d5d5f5db55162e0ef56aeddde6b
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-17 18:52:47 +0100

    Removed using statements

--- src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
+++ src/ApiGateways/Shopping.Aggregator/Services/OrderService.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
-using Shopping.Aggregator.Extensions;
+﻿using Shopping.Aggregator.Extensions;
 using Shopping.Aggregator.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
