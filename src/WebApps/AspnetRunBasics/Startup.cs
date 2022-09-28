fc762f55e2c34166ad50266c257d6faaf51cc643
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-09-28 21:33:26 +0100

    -ICartRepository

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -24,7 +24,7 @@ namespace AspnetRunBasics
 
             // add repository dependecy
             services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped<ICartRepository, CartRepository>();
+            //services.AddScoped<ICartRepository, CartRepository>();
             //services.AddScoped<IOrderRepository, OrderRepository>();
             //services.AddScoped<IContactRepository, ContactRepository>();
 
