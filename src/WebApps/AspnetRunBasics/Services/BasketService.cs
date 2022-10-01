c16590700fce18ecfac6eeb886195ac701ca0659
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-02 00:08:06 +0100

    Injected httpClient

--- src/WebApps/AspnetRunBasics/Services/BasketService.cs
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -2,12 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class BasketService : IBasketService
     {
+        private readonly HttpClient _client;
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
         public Task CheckOutBasket(BasketCheckoutModel model)
         {
             throw new NotImplementedException();
