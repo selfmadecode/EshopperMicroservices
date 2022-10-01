3609b546f57edeab6b0a1d377512a4b6b61163cc
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-02 00:20:07 +0100

    implemeted GetOrdersByUserName

--- src/WebApps/AspnetRunBasics/Services/OrderService.cs
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -12,6 +14,12 @@ namespace AspnetRunBasics.Services
         public OrderService(HttpClient client)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        {
+            var response = await _client.GetAsync($"/Order/{userName}");
+            return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
 }
