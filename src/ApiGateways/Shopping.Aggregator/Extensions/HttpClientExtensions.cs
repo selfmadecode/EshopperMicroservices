9886e7aadc9aaa02e1f9dcd5f938d556050c9135
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-01 23:39:39 +0100

    Added PostAsJson

--- src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
+++ src/ApiGateways/Shopping.Aggregator/Extensions/HttpClientExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -15,6 +16,15 @@ namespace Shopping.Aggregator.Extensions
             var dataAsString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
 
             return JsonSerializer.Deserialize<T>(dataAsString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+
+        public static Task<HttpResponseMessage> PostAsJson<T>(this HttpClient httpClient, string url, T data)
+        {
+            var dataAsString = JsonSerializer.Serialize(data);
+            var content = new StringContent(dataAsString);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return httpClient.PostAsync(url, content);
         }
     }
 }
