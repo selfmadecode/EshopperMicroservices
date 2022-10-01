225681ecde573676456d449d944ccb8ed2ede28f
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-02 00:19:21 +0100

    Injected HttpCliebt in OrderService

--- src/WebApps/AspnetRunBasics/Services/OrderService.cs
+++ src/WebApps/AspnetRunBasics/Services/OrderService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
     public class OrderService : IOrderService
     {
+        private readonly HttpClient _client;
+        public OrderService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
     }
 }
