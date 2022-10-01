c2ee5e2e219f942b500d9c3b5d3ec193e5aaadc5
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-02 00:03:09 +0100

    Implemented GetCalalog

---
+++ src/WebApps/AspnetRunBasics/Services/CatalogService.cs
@@ -0,0 +1,32 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient _client;
+        public CatalogService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+        public Task<CatalogModel> CreateCatalog(CatalogModel product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            // Call /Catalog on the gateway which will redirect to catalog api
+            var response = await _client.GetAsync("/Catalog");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        
+    }
+}
