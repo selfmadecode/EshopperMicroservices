f0ec8d7fe7cc51ad75c645b71f07803fcddab912
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-01 23:28:27 +0100

    Inherited and implemented IBasket

--- src/WebApps/AspnetRunBasics/Services/BasketService.cs
+++ src/WebApps/AspnetRunBasics/Services/BasketService.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using AspnetRunBasics.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspnetRunBasics.Services
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
+        public Task CheckOutBasket(BasketCheckoutModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketModel> GetBasket(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
