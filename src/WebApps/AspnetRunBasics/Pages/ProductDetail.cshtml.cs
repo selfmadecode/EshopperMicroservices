baf2eae4e03bde9fbb8d9fea76a3e63cf1787474
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-05 22:50:58 +0100

    Refactore ProductDetails

--- src/WebApps/AspnetRunBasics/Pages/ProductDetail.cshtml.cs
+++ src/WebApps/AspnetRunBasics/Pages/ProductDetail.cshtml.cs
@@ -26,14 +26,14 @@ namespace AspnetRunBasics
         [BindProperty]
         public int Quantity { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? productId)
+        public async Task<IActionResult> OnGetAsync(string productId)
         {
             if (productId == null)
             {
                 return NotFound();
             }
 
-            Product = await _productRepository.GetProductById(productId.Value);
+            Product = await _catalogService.GetCatalog(productId);
             if (Product == null)
             {
                 return NotFound();
@@ -41,12 +41,23 @@ namespace AspnetRunBasics
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAddToCartAsync(int productId)
+        public async Task<IActionResult> OnPostAddToCartAsync(string productId)
         {
-            //if (!User.Identity.IsAuthenticated)
-            //    return RedirectToPage("./Account/Login", new { area = "Identity" });
+            var product = await _catalogService.GetCatalog(productId);
 
-            await _cartRepository.AddItem("test", productId, Quantity, Color);
+            var userName = "swn";
+            var basket = await _basketService.GetBasket(userName);
+
+            basket.Items.Add(new BasketItemExtendedModel
+            {
+                ProductId = productId,
+                ProductName = product.Name,
+                Price = product.Price,
+                Quantity = 1,
+                Color = "Black"
+            });
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
             return RedirectToPage("Cart");
         }
     }
