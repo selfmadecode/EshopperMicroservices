f134dc0daf83c67eb7f740a47acdf0c06a58b8a7
Anyanwu Raphael <anyanwuraphaelc@gmail.com>
2022-10-08 22:59:50 +0100

    Removed unused using

--- src/WebApps/AspnetRunBasics/Models/OrderResponseModel.cs
+++ src/WebApps/AspnetRunBasics/Models/OrderResponseModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AspnetRunBasics.Models
+﻿namespace AspnetRunBasics.Models
 {
     public class OrderResponseModel
     {
