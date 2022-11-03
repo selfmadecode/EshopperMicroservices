bf1337b2a4bae1653cafaaca35d18eb60f28614a
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-11-03 17:43:26 +0100

    Removed configure comments on startup

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -16,7 +16,6 @@ namespace IdentityServer
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
