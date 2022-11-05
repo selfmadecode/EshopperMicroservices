c688d5884338b1c041ab89ee04e10d898d0480b8
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-11-06 00:01:25 +0100

    Registered dbcontext

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -1,16 +1,33 @@
+using IdentityServer.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityServer
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                // Configure Entity Framework Core to use Microsoft SQL Server.
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+
+                // Register the entity sets needed by OpenIddict.
+                options.UseOpenIddict(); // add the OpenIddict entity sets to DbContext
+            });
             services.AddOpenIddict();
         }
 
