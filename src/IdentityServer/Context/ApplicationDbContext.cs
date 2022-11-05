c810a58244b7f3a19c78d92bcd875750e13931b0
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-11-05 22:40:56 +0100

    Added ApplicationDbContext

---
+++ src/IdentityServer/Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+    }
+}
