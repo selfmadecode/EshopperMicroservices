9436f517262ccc037121368426fa6ee9e002ab35
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2022-11-05 22:50:03 +0100

    Updated dbcontext

--- src/IdentityServer/Context/ApplicationDbContext.cs
+++ src/IdentityServer/Context/ApplicationDbContext.cs
@@ -8,5 +8,9 @@ namespace IdentityServer.Context
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
     }
 }
